0c390791286a6093c04015c02469d960bd901133
hermmanhender <hendger.07@gmail.com>
2022-07-07 09:44:19 -0300

    .

--- EPRLlib_client.py
+++ EPRLlib_client.py
@@ -563,8 +563,8 @@ config = {'Folder_Output': '',
         'dT_up': 1.,
         'dT_dn': 4.,
         'SP_RH': 70.,
-        'nombre_caso': "fanger_comfort", # Se utiliza para identificar la carpeta donde se guardan los datos
-        'rho': 10, # Temperatura: default: 0.25
+        'nombre_caso': "rho-5", # Se utiliza para identificar la carpeta donde se guardan los datos
+        'rho': 5, # Temperatura: default: 0.25
         'beta': 1, # Energía: default: 20
         'psi': 0, # Humedad relativa: default: 0.005
         'first_time_step': True,
